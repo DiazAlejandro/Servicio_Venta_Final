b716d1b8cf83e2a097834575556feb64739c77f0
DiazAlejandro <alejandro_diaz1253@hotmail.com>
2022-05-29 23:55:13 -0500

    Configuración inicial

---
+++ src/main/java/edu/mx/tecnm/oaxaca/servicioventa/ServicioventaApplication.java
@@ -0,0 +1,13 @@
+package edu.mx.tecnm.oaxaca.servicioventa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioventaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioventaApplication.class, args);
+	}
+
+}
