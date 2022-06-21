df3fff6bb0a311370439d571039393ae84dd85b7
DiazAlejandro <alejandro_diaz1253@hotmail.com>
2022-06-20 19:20:33 -0500

    Validacion actualizacion

--- src/main/java/edu/mx/tecnm/oaxaca/servicioventa/controller/VentaController.java
+++ src/main/java/edu/mx/tecnm/oaxaca/servicioventa/controller/VentaController.java
@@ -155,7 +155,7 @@ public class VentaController {
                 return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
                         new CustomResponse("JWT invalid or expired", 401));
             }
-            if (!(idVenta+"").matches("-?\\d+")){
+            if (!(idVenta + "").matches("-?\\d+")) {
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
                         new CustomResponse("Param can not be a strrng", 400));
             }
@@ -176,19 +176,59 @@ public class VentaController {
             );
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(
-                    new CustomResponse(HttpStatus.UNPROCESSABLE_ENTITY, 
+                    new CustomResponse(HttpStatus.UNPROCESSABLE_ENTITY,
                             e.getMessage().toString(), 422));
         }
     }
 
     @PutMapping("/venta/{idVenta}")
-    public CustomResponse updateVenta(@RequestBody VentaModel venta, @PathVariable Integer idVenta) {
+    public ResponseEntity<Object> updateVenta(@RequestHeader(value = "Authorization", required = false) String authorization,
+            @RequestBody VentaModel venta, @PathVariable Integer idVenta) {
+        ResponseEntity<Object> responseEntity = null;
         CustomResponse customResponse = new CustomResponse();
-        ventaService.updateVenta(venta, idVenta);
-        customResponse.setHttpCode(HttpStatus.NO_CONTENT);
-        customResponse.setCode(204);
-        customResponse.setMensaje("Update Success");
-        return customResponse;
+
+        try {
+            if (authorization == null) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
+                        new CustomResponse(HttpStatus.UNAUTHORIZED,
+                                "Please, send a JWT Headers like Authorization",
+                                401));
+            }
+            if (!auth.verifyToken(authorization)) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
+                        new CustomResponse("JWT invalid or expired", 401));
+            }
+            if ((venta.getCambio() + "").isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NO_CONTENT).body(
+                        new CustomResponse("Process invalid", 204));
+            }
+            if (venta.getCostoTotal() > venta.getCantidadPagada()) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                        new CustomResponse("La cantidad a pagar tiene que ser mayor al costo total", 400));
+            }
+            if (venta.getCambio() > venta.getCantidadPagada()) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                        new CustomResponse("El cambio no puede ser mayor que la cantidad pagada", 400));
+            }
+            if (!(venta.getRfc().length() == 13)) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                        new CustomResponse("La longitud del RFC tiene que ser 13", 400));
+            }
+            ventaService.updateVenta(venta, idVenta);
+            customResponse.setHttpCode(HttpStatus.NO_CONTENT);
+            customResponse.setCode(204);
+            customResponse.setMensaje("Update Success");
+        } catch (DataIntegrityViolationException e) {
+            customResponse.setMensaje("Error with ID");
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(customResponse);
+        } catch (HttpClientErrorException e) {
+            customResponse.setMensaje("JWT invalid or expired");
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(customResponse);
+        } catch (Exception e) {
+            customResponse.setMensaje(e.getMessage());
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(customResponse);
+        }
+        return responseEntity;
     }
 
     @DeleteMapping("/venta/{idVenta}")
